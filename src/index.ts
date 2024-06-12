9e8372fc4554034435ac9c1331380e27974b7783
Evgeny Archi <e.archibasov@gmail.com>
2024-06-12 18:15:16 +0700

    Добавить енвы и уточнить скрипты

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
-'use strict';
 import * as net from 'node:net';
 
+interface Data {
+    name: string;
+    text: string;
+}
+
 const server = net.createServer(socket => {
-    socket.on('data', data => {
-        socket.write(data);
+    socket.on('data', (data: Data) => {
+        console.log(data);
+        // const request = JSON.parse(data);
+        // socket.write(request.text);
+        // console.log('receive data: ', request);
     });
     socket.on('close', () => {
         socket.end();
     });
 });
 
-server.on('connection', socket => {
-    console.log('client connected', process.pid);
+server.on('connection', () => {
+    console.log('Client connected', process.pid);
 });
 
-server.listen(60300, () => {
-    console.log('Listen 60300');
+const port = process.env.PORT || 0;
+const isDEV = process.env.DEV;
+
+server.listen(port, () => {
+    console.log('Server started...');
+    console.log('Mode:', isDEV ? 'DEV' : 'PROD');
+    console.log('Address:', server.address());
 });
